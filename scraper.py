689ed0f7898804c6dbb013b80b1821d489633005
Alessio Michelini <darkmavis1980@gmail.com>
2022-10-31 10:27:33 +0000

    Finished code

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 import scrapy
 import requests
 import os
+import json
 
 class BrickSetSpider(scrapy.Spider):
     name = "brickset_spider"
@@ -12,7 +13,7 @@ class BrickSetSpider(scrapy.Spider):
     }
 
     def start_requests(self):
-        urls = ['https://brickset.com/sets/theme-Collectable-Minifigures/page-{}'.format(x) for x in range(1, 4)]
+        urls = ['https://brickset.com/sets/theme-Collectable-Minifigures/page-{}'.format(x) for x in range(1, 32)]
         # urls = [
         #     'https://brickset.com/sets/theme-Collectable-Minifigures/page-1',
         #     'https://brickset.com/sets/theme-Collectable-Minifigures/page-2',
@@ -21,36 +22,59 @@ class BrickSetSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        # page = response.url.split("/")[-1]
-        # filename = f'quotes-{page}.html'
-        # print(response.body)
-
-        # with open(filename, 'wb') as f:
-        #     f.write(response.body)
-        # self.log(f'Saved file {filename}')
-
         SET_SELECTOR = '.set'
         for brickset in response.css(SET_SELECTOR):
             NAME_SELECTOR = 'h1 a::text'
             name = brickset.css(NAME_SELECTOR).get()
-            if not name.startswith(' LEGO Minifigures'):
+            if not name.startswith(' LEGO Minifigures', ' Collectable Minifigures series'):
                 image = brickset.css('.mainimg img').attrib['src']
+                link = "https://brickset.com{}".format(brickset.css('h1 a').attrib['href'])
                 tags = brickset.css('.col .tags a::text').get()
+                minifigId = brickset.css('.meta .tags a::text').get()
                 series = brickset.css('.meta .tags .subtheme::text').get()
+                year = brickset.css('.meta .tags .year::text').get()
 
                 imageFileName = image.split('?')[0].split('/')[-1]
-                imageDir = "images/{}".format(series)
+                '''
+                Save Image file
+                '''
+                imageDir = "images/{}".format(series.strip())
                 imageRequest = requests.get(image)
 
                 if not os.path.exists(imageDir):
                     os.makedirs(imageDir)
 
-                with open("{}/{}-{}".format(imageDir, tags, imageFileName), 'wb') as f:
-                    f.write(imageRequest.content)
-                yield {
+                with open("{}/{}-{}".format(imageDir, tags, imageFileName), 'wb') as imageFile:
+                    imageFile.write(imageRequest.content)
+                '''
+                Save JSON file
+                '''
+                dataDir = "data/{}".format(series.strip())
+                if not os.path.exists(dataDir):
+                    os.makedirs(dataDir)
+                minifigSet = {
                     'name': name,
+                    'minifigId': minifigId,
+                    'link': link,
                     'image': image,
                     'imageFileName': imageFileName,
                     'tags': tags,
-                    'series': series
+                    'series': series,
+                    'year': year
+                }
+
+                minifigJson = json.dumps(minifigSet)
+
+                with open("{}/{}-{}.json".format(dataDir, tags, minifigId), 'w') as outJson:
+                    outJson.write(minifigJson)
+
+                yield {
+                    'name': name,
+                    'minifigId': minifigId,
+                    'link': link,
+                    'image': image,
+                    'imageFileName': imageFileName,
+                    'tags': tags,
+                    'series': series,
+                    'year': year
                 }
